4610d1e0a595215314fa038da05846970a1b42fe
kasmturny <wzzsama@outlook.com>
2024-10-24 17:05:08 +0800

    1

---
+++ model/bge.py
@@ -0,0 +1,22 @@
+import os
+from typing import List
+from langchain_community.embeddings.huggingface import HuggingFaceBgeEmbeddings
+
+class OpenAIBgeEmbeddings:
+    def __init__(self):
+        model_name = "./libs/bge-large-zh-v1.5"
+        model_kwargs = {'device': 'cpu'}
+        encode_kwargs = {'normalize_embeddings': True}
+        self.model = HuggingFaceBgeEmbeddings(
+            model_name=os.path.join(os.path.dirname(__file__), model_name),
+            model_kwargs=model_kwargs,
+            encode_kwargs=encode_kwargs,
+        )
+
+    def get_embedding(self, query: str) -> List[float]:
+        vector = self.model.embed_query(query)
+        return vector
+
+if __name__ == '__main__':
+    test = OpenAIBgeEmbeddings().get_embedding("你好")
+    print(test)
