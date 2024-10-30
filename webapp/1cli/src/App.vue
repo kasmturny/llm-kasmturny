a93ff314b3dae4b1745513b844346e51164ac9d9
kasmturny <wzzsama@outlook.com>
2024-10-31 01:32:22 +0800

    kasmturny

--- webapp/1cli/src/App.vue
+++ webapp/1cli/src/App.vue
@@ -22,26 +22,10 @@ export default {
     goToChatBot() {
       // 检查当前路由是否已经是 /chatbot
       if (this.$route.path !== '/chatbot') {
-        this.$router.push('/chatbot').then(() => {
-          // 路由跳转成功后发送 POST 请求
-          fetch('http://localhost:8000/newchat', {
-            method: 'POST',
-            headers: {
-              'Content-Type': 'application/json'
-            }
-          })
-              // eslint-disable-next-line no-unused-vars
-          .then(response => {
-            // 请求成功，处理响应
-          })
-          .catch(error => {
-            console.error('Error sending request:', error);
-            // 请求失败，处理错误
-          });
-        });
+        this.$router.push('/chatbot')
+        // 路由跳转成功后发送 POST 请求
       }
     }
-
   }
 }
 </script>
